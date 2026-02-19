f0c42dfed216f6b74448aa5fae2a2a9cea76b721
Ponnagaraj-K <727723euci036@skcat.ac.in>
2026-02-19 12:59:24 +0530

    first commit

---
+++ mul.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String ags[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int r ;
+        r=a*b;
+        System.out.print(r);
+    }
+}
